d2e0234ea1576768b49a11473bf8c6cd5655b338
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-02-22 16:13:29 +0530

    logs

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/health', (req, res) => {
 app.post('/webhook', express.json(), async (req, res) => {
     let body = req.body;
 
-    console.log("Changes: ", util.inspect(body.entry[0].changes, {depth: null, colors: true}));
+    console.log("Changes: ", JSON.stringify(body.entry[0].changes, null, 2));
 
     if (body.object) {
         if (body.entry &&
