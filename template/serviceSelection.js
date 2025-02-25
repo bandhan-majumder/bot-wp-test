ef325550b7ea6bdd9cfb2a7b7c17bcc39db94f01
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-02-25 18:52:31 +0530

    update

--- template/serviceSelection.js
+++ template/serviceSelection.js
@@ -4,7 +4,7 @@ module.exports = async function sendServiceSelectionTemplate() {
     try {
         const response = await axios({
             method: 'POST',
-            url: 'https://graph.facebook.com/v22.0/572643735931375/messages',
+            url: 'https://graph.facebook.com/v21.0/572643735931375/messages',
             headers: {
                 'Authorization': `Bearer ${process.env.TOKEN}`,
                 'Content-Type': 'application/json'
